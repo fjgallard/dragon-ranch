4cd69d19985d2aa2c6c71903e312aeb6032baaaf
Francis Jomer Gallardo <fjdgallardo@gmail.com>
2023-08-15 12:43:52 +0800

    Initial commit

---
+++ apps/dragon-ranch/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dragon-ranch-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'dragon-ranch';
+}
