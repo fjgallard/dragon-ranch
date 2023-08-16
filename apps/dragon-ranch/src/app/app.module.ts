333bade2608f38959831d99cc5d07bb3384da75f
Francis Jomer Gallardo <fjdgallardo@gmail.com>
2023-08-16 08:05:09 +0800

    Setup Angular Material.

--- apps/dragon-ranch/src/app/app.module.ts
+++ apps/dragon-ranch/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@dragon-ranch/material';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    MaterialModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ libs/material/src/lib/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+@NgModule({
+  imports: [CommonModule, MatCheckboxModule],
+  exports: [
+    MatCheckboxModule
+  ]
+})
+export class MaterialModule {}
